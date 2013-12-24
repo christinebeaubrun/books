af5b7cda0888b890c469844829085576d4496e4c
Christine Beaubrun <christinebeaubrun@gmail.com>
2013-12-24 13:06:35 -0500

    New line in ex1

--- ex1.rb
+++ ex1.rb
@@ -6,3 +6,5 @@ puts #'Yay! Printing.'
 puts #"I'd much rather you 'not'."
 puts #'I "said" do not touch this.'
 puts "I am programming! I'm so excited and I just can't hide it!"
+puts "So I'm trying this new thing called GitHub . . ."
+
